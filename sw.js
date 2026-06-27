9cbbe360ebf41da1fafe09f81b30efc24faa62d9
dsasanim4-source <dsasanim4@gmail.com>
2026-06-27 22:49:07 +0800

    Add update prompt and owned wish star controls

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = 'wish-upon-a-star-v8';
+const CACHE_NAME = 'wish-upon-a-star-v9';
 const APP_SHELL = [
   './',
   './index.html',
@@ -12,8 +12,13 @@ self.addEventListener('install', event => {
   event.waitUntil(
     caches.open(CACHE_NAME)
       .then(cache => cache.addAll(APP_SHELL))
-      .then(() => self.skipWaiting())
   );
+});
+
+self.addEventListener('message', event => {
+  if (event.data && event.data.type === 'SKIP_WAITING') {
+    self.skipWaiting();
+  }
 });
 
 self.addEventListener('activate', event => {
